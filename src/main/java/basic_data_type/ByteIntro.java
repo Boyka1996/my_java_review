0b9dd796ce4c90c2a515761a7f2960c0a5ca8f8e
boyka <upcvagen@163.com>
2020-07-13 16:49:46 +0800

    Basic data type byte

---
+++ src/main/java/basic_data_type/ByteIntro.java
@@ -0,0 +1,10 @@
+package basic_data_type;
+
+public class ByteIntro {
+    public static void main(String[] args) {
+        byte number = 55;
+        byte x = 10 + 15;
+        System.out.println("Byte类型变量number为："+number);
+        System.out.println("Byter类型变量x为："+x);
+    }
+}
